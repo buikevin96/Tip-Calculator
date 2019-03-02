ebba164e6208795f9a32e4cdd3e859ccaeaef008
buikevin96 <buikevin96@gmail.com>
2019-03-01 20:50:42 -0500

    Added calculate function and math logic

--- app/src/main/java/com/example/kevinbui/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/kevinbui/tipcalculator/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button calculateButton;
     private TextView totalResultTextView;
     private TextView textViewSeekBar;
+    private int seekBarPercentage;
+    private float enteredBillFloat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +30,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         totalResultTextView = (TextView)findViewById(R.id.resultID);
         textViewSeekBar = (TextView)findViewById(R.id.textViewSeekBar);
 
+
         calculateButton.setOnClickListener(this);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                textViewSeekBar.setText(String.valueOf(seekBar.getProgress()) + "%");
+                textViewSeekBar.setText(String.valueOf(seekBar.getProgress()) + "%"); // Changes the value of the textView
             }
 
             @Override
@@ -44,12 +48,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+                seekBarPercentage = seekBar.getProgress(); // Holds the value of the seekBar
             }
         });
     }
 
     @Override
     public void onClick(View v){
+        calculate();
+    }
+
+    // calculate logic
+    public void calculate(){
+        float result = 0.0f;
+
+        // Checks if there is a value in for the bill amount
+        if (!enteredAmount.getText().toString().equals("")) {
+
+            enteredBillFloat = Float.parseFloat(enteredAmount.getText().toString()); // Converts the text to float and hold the value
+            result = enteredBillFloat * seekBarPercentage / 100; // Multiply entered bill amount by pecentage of the seekBar
+            totalResultTextView.setText("Your tip will be $" + String.valueOf(result));
+
+        } else {
+            Toast.makeText(MainActivity.this, "Please enter a bill amount.", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
