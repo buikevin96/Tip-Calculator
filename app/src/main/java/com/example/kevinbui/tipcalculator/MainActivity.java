497cf544129a8e15cd8fc5862de1bd7ae235d874
buikevin96 <buikevin96@gmail.com>
2019-03-01 20:31:11 -0500

    Added some Logic, Added additional textviews

--- app/src/main/java/com/example/kevinbui/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/kevinbui/tipcalculator/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.kevinbui.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText enteredAmount;
+    private SeekBar seekBar;
+    private Button calculateButton;
+    private TextView totalResultTextView;
+    private TextView textViewSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enteredAmount = (EditText)findViewById(R.id.billAmountID);
+        seekBar = (SeekBar)findViewById(R.id.percentageSeekBar);
+        calculateButton = (Button)findViewById(R.id.calculateButton);
+        totalResultTextView = (TextView)findViewById(R.id.resultID);
+        textViewSeekBar = (TextView)findViewById(R.id.textViewSeekBar);
+
+        calculateButton.setOnClickListener(this);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                textViewSeekBar.setText(String.valueOf(seekBar.getProgress()) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v){
+
     }
 }
